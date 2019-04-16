7f3c726ce12e56dc515cd66e5a1b98f793de6f6c
Włodzimierz Kozłowski <wlodzimierz@kozlowscy.org>
2019-04-16 16:49:31 +0200

    aktualizacja logo, dodanie opisu

--- flask_app.py
+++ flask_app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'To jest mikrousluga, ktora zwraca menu!'
-	
+
 @app.route('/about')
 def about():
 	return 'pykebab-menu (kebab 2.1)'
-	
+
 @app.route('/menu')
 def menu():
 	client = ScrapinghubClient(config['scrapinghub']['api_key'])
 	project = client.get_project(config['scrapinghub']['project_id'])
-	job = project.jobs.list(spider=config['scrapinghub']['spider_name'], state='finished', count=1)[0]		
+	job = project.jobs.list(spider=config['scrapinghub']['spider_name'], state='finished', count=1)[0]
 	job = client.get_job(job['key'])
-	
+
 	menu = {}
 	menu['aktualnosc'] = job.metadata.get('finished_time')
 	menu['restauracja'] = {
 		"nazwa": "CamelPizza",
-		"logo": "http://camelpizza.pl/wp-content/uploads/2014/06/logo.png",
+		"logo": "https://www.camelpizza.pl/system/logos/27323/menu_size/1549450693.png",
 		"url": "http://camelpizza.pl"
 	}
 	menu['grupy'] = []
@@ -41,7 +41,7 @@ def menu():
 		for pozycja in grupa['pozycje']:
 			if pozycja['nazwa'] == item['pozycja']:
 				return pozycja
-		pozycja = { 'nazwa': item['pozycja'], 'warianty': [] }
+		pozycja = { 'nazwa': item['pozycja'], 'opis': item['opis'], 'warianty': [] }
 		grupa['pozycje'].append(pozycja)
 		return pozycja
 
@@ -51,11 +51,11 @@ def menu():
 		waluta = waluta.replace(u'zł', 'PLN')
 		return { 'kwota': kwota, 'waluta': waluta }
 
-	items = job.items.list()		
+	items = job.items.list()
 	for item in items:
 		pozycja = get_pozycja(item)
 		wariant = { 'opis': item['wariant'], 'ceny': [ get_cena(item) ]}
-		pozycja['warianty'].append(wariant)	
+		pozycja['warianty'].append(wariant)
 
 	return jsonify(menu)
 	#return json.dumps(menu), 200, 'application/json'
